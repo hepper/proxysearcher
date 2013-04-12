e02c7154c8e10e45062a0708de2d4fd7847ab5ee
Roman Burda <burda_r@mail.ru>
2013-04-13 00:46:33 +0300

    Reworked design

--- ProxySearch.Application/Code/Converters/BandwidthResultColorConverter.cs
+++ ProxySearch.Application/Code/Converters/BandwidthResultColorConverter.cs
@@ -23,14 +23,14 @@ namespace ProxySearch.Console.Code.Converters
                 return string.Empty;
 
             if (state == BandwidthState.Error)
-                return Brushes.Red;
+                return new SolidColorBrush(Color.FromArgb(128, 0xFF, 0, 0));
 
             if (bandwidth > maxBandwidth)
                 bandwidth = maxBandwidth;
 
             byte color = (byte)((255 * bandwidth.Value) / maxBandwidth);
 
-            return new SolidColorBrush(Color.FromRgb((byte)(255 - color), color, 0));
+            return new SolidColorBrush(Color.FromArgb(128, (byte)(255 - color), color, 0));
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
